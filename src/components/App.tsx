8d208ad3d4ce30a65dbffe0fc3ef5e27cf0b5d5a
brunohsn6 <bruno.hsn6@gmail.com>
2021-03-07 14:34:22 -0300

    configuring initial architecture of the react application

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import '../assets/styles/App.scss';
+export default class App extends Component{
+    render(){
+        return (
+        <div className="App">
+            <h1>Hello World</h1>
+        </div>
+        );
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '.ts', '.ts', '.tsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        use: {
+          loader: 'awesome-typescript-loader',
+        },
+      },
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        use: {
+          loader: 'source-map-loader',
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: './src/assets/styles/App.scss',
+    }),
+  ],
+};
